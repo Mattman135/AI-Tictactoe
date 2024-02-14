4d0f1c7bd893ce70399c6468d08820c5b11be789
Mattis Hallberg <skatemattis97@hotmail.com>
2024-02-14 14:07:48 +0100

    Set up the game so that it should be straighforward to implement the minimax algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,113 @@
-/*function component() {
-  
-  return element
-}
+const Gameboard = (() => {
+  const infoDisplay = document.querySelector("#info")
+  const gameBoard = document.querySelector("#gameboard")
+  const cellElements = ["", "", "", "", "", "", "", "", ""]
+  const restartBtn = document.querySelector("#restartBtn")
 
-document.body.appendChild(component())*/
+  let round = 1
+  let go = "cross"
+  infoDisplay.textContent = `It's round ${round} and ${go}'s turn`
+
+  restartBtn.addEventListener("click", () => {
+    deleteBoard(gameBoard)
+    createBoard()
+    round = 0
+    go = "cross"
+    infoDisplay.textContent = `It's round ${round} and ${go}'s turn`
+  })
+
+  const deleteBoard = (parent) => {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild)
+    }
+  }
+
+  const createBoard = () => {
+    cellElements.forEach((_cell, index) => {
+      const div = document.createElement("div")
+      div.classList.add("square")
+      div.id = index
+      div.addEventListener("click", addGo)
+      gameBoard.append(div)
+    })
+  }
+
+  const addGo = (e) => {
+    const div2 = document.createElement("div")
+    div2.classList.add(go)
+    e.target.append(div2)
+    go = go === "circle" ? "cross" : "circle"
+    round++
+    infoDisplay.textContent = `It's round ${round} and ${go}'s turn`
+
+    // När det är datorns tur så ska jag anropa BestMove här
+    // sedan printar jag circle i den diven som best move är
+    // tar bort addeventlistener (så att jag inte kan klicka på den)
+    // och kollar ifall vi har en vinnare.
+
+    e.target.removeEventListener("click", addGo)
+    checkWinner()
+  }
+
+  const checkWinner = () => {
+    const allSquares = document.querySelectorAll(".square")
+
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    winCombinations.forEach((arr) => {
+      const crossWins = arr.every((cell) =>
+        allSquares[cell].firstChild?.classList.contains("cross")
+      )
+
+      if (crossWins) {
+        infoDisplay.textContent = "Cross wins"
+        allSquares.forEach((square) =>
+          square.replaceWith(square.cloneNode(true))
+        )
+        return
+      } else if (round === 10 && !crossWins) {
+        infoDisplay.textContent = `It's a draw`
+        allSquares.forEach((square) =>
+          square.replaceWith(square.cloneNode(true))
+        )
+        return
+      }
+    })
+
+    winCombinations.forEach((arr) => {
+      const circleWins = arr.every((cell) =>
+        allSquares[cell].firstChild?.classList.contains("circle")
+      )
+
+      if (circleWins) {
+        infoDisplay.textContent = "Circle wins"
+        allSquares.forEach((square) =>
+          square.replaceWith(square.cloneNode(true))
+        )
+        return
+      } else if (round === 10 && !circleWins) {
+        infoDisplay.textContent = `It's a draw`
+        allSquares.forEach((square) =>
+          square.replaceWith(square.cloneNode(true))
+        )
+        return
+      }
+    })
+  }
+
+  return { createBoard }
+})()
+
+const displayController = (() => {
+  const Board = Gameboard
+  Board.createBoard()
+})()
